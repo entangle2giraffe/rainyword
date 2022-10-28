12cf1246a86161ce6a673ff9f28273a1de6585ff
ThaksinGaPikattoHikattara <ballplaysmc@gmail.com>
2022-10-28 21:15:59 +0700

    Update lobby.py

--- lobby.py
+++ lobby.py
@@ -1,13 +1,22 @@
-from utils import json_read, save_to_file
+from http import client
+from utils import json_read, jsonify, save_to_file
 import player
 
-#convert string to json
-#when 2 clients are ready, send player json
+def read_status(data:str) -> bool:
+    """Read status of json string if both
+    of them are ready (1) then return true"""
+    data = jsonify(data)
+    if data['Status']['player1']['ready'] == 1 and data['Status']['player2']['ready'] == 1:
+        return True
+    else: return False
 
-data =[{"IP":"198.0.0.1","Ready":"1"},{"IP":"198.0.0.2","Ready":"1"}]
+def return_player(res:bool) -> None:
+    """"If read_status() == True then return json_player from playr.py"""
+    if res == True:
+        return player.json_players(data['Status']['player1'],data['Status']['player2'])
 
-def delete():
-    pass
-
-def string_to_json():
-    save_to_file(client_response, data)
+if __name__ == "__main__":
+    data = {'Status': {'player1': 1}, {'player2' : 2}}
+    #res = read_status(data)
+    #return_player(res)
+    print(type(data))
