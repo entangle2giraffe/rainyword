209e24877c27fcb55f9c58cb919414b86d97ff37
hxfe <bittersalad@tutanota.de>
2022-10-28 22:03:45 +0700

    Update lobby.py

--- lobby.py
+++ lobby.py
@@ -1,22 +1,24 @@
-from http import client
 from utils import json_read, jsonify, save_to_file
 import player
+from Object import Player
+import json
 
 def read_status(data:str) -> bool:
     """Read status of json string if both
     of them are ready (1) then return true"""
-    data = jsonify(data)
-    if data['Status']['player1']['ready'] == 1 and data['Status']['player2']['ready'] == 1:
+    data = json.loads(data)
+    if data['Status'][0]['player'] == 1 and data['Status'][1]['player'] == 1:
         return True
     else: return False
 
-def return_player(res:bool) -> None:
+def return_player(res:bool):
     """"If read_status() == True then return json_player from playr.py"""
     if res == True:
-        return player.json_players(data['Status']['player1'],data['Status']['player2'])
+        p1 = Player(1)
+        p2 = Player(2)
+        data = player.json_players(p1,p2)
+        print(data)
 
 if __name__ == "__main__":
-    data = {'Status': {'player1': 1}, {'player2' : 2}}
-    #res = read_status(data)
-    #return_player(res)
-    print(type(data))
+    data = '{"Status": [{"player":1},{"player":1}]}'
+    return_player(read_status(data))
