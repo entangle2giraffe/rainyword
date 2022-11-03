dfa9b0c93800df75aa22650e22732e9a71a0e5a4
ThaksinGaPikattoHikattara <ballplaysmc@gmail.com>
2022-11-03 22:18:52 +0700

    Update expired_word.py

--- expired_word.py
+++ expired_word.py
@@ -1,10 +1,15 @@
 # -> {"WordExpired":"word"}
 # {"WordRemoved":"word"} ->
+import json
 
 def remove_word(data:str): # Example of how to read json str in lobby.py
     """
     Read JSON WordExpired and return WordRemoved
     """
+    data['WordRemoved'] = data["word"]
+    return data
 
 if __name__ == "__main__":
-    pass
+    data = {"word": ["stable", "tgp", "complex", "professionals", "advocate"]}
+    remove_word(data)
+    print(data)
