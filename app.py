2e5133f57a7ce4b52c9bccca2c8e3d56bcec2691
hxfe <bittersalad@tutanota.de>
2022-11-06 18:52:10 +0700

    OOP changed

--- app.py
+++ app.py
@@ -1,41 +1,72 @@
 import socket
 import time
-import configparser
-import time
 import logging
-from player import json_players
+from sender.player import json_players
 import Object
 import os
 from _thread import *
+from sender.assign_id import return_id
 
-# Logger Config
-logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(asctime)s %(message)s')
+class Server:
+    connections = []
+    addresses = []
+    thread_count = 0
 
-def _read_config() -> int:
-    config = configparser.ConfigParser()
-    config.read("config") # Read config file
-    port = config.get('server','port')
-    
-    return port
+    def __init__(self, port:int):        
+        logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(asctime)s %(message)s')
+        self.port = port
+  
+    def multi_threaded_client(self):
+        """
+        Connect Multiple CLients in Python
+        """
+        FORMAT = "utf-8"
+        DISCONNECT_MESSAGE = "!DISCONNECT" # [NOT IMPLEMENT]
+        #connection.send(str.encode(f'Server is working:'))
+        #data = connection.recv(2048)
+        #logging.info(f'{address} '+data.decode(FORMAT))
+        #response = client_init()
+        #connection.sendall(f'{response}'.encode(FORMAT))
+        self.c.sendall(f'{return_id(self.thread_count)}'.encode(FORMAT))
+        while True:
+            data = self.c.recv(2048)
+            response = '[SERVER] ' +data.decode(FORMAT)
+            if not data:
+                break
+            try:
+                self.c.sendall(str.encode(response))
+            except:
+                break
 
-def start(client_n=2):
-    """
-    Open up TCP Socket Server
-    """
-    port = _read_config() # Read config file
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Initialize TCP
-    try:
-        sock.bind(('',int(port)))
-    except socket.error as e:
-        logging.error(f"{str(e)}")
-    logging.info(f"socket is binded to {port}")
+        self.c.close()
 
-    # listen to n clients 
-    sock.listen(client_n)
-    logging.info("socket is listening")
-    
-    return sock
+    def start(self, client_n:int=2):
+        """
+        Open up TCP Socket Server
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Initialize TCP
+        try:
+            sock.bind(('',self.port))
+        except socket.error as e:
+            logging.error(f"{str(e)}")
+        logging.info(f"socket is binded to {self.port}")
 
+        # listen to n clients 
+        sock.listen(client_n)
+        logging.info("socket is listening")
+        while True:
+            try:
+                self.c, self.addr = sock.accept()
+            except:
+                break
+            self.connections.append(self.c)
+            self.addresses.append(self.addr) 
+            logging.debug("Connection from: "+str(self.addr))
+            start_new_thread(self.multi_threaded_client,())
+            self.thread_count += 1
+            logging.debug(f"Thread: {self.thread_count}")
+        sock.close()
+        
 def client_init():
     """
     When client send first two data about players
@@ -45,37 +76,7 @@ def client_init():
     data = json_players(p1,p2)
     return data
 
-def multi_threaded_client(connection, address):
-    """
-    Connect Multiple CLients in Python
-    """
-    FORMAT = "utf-8"
-    DISCONNECT_MESSAGE = "!DISCONNECT" # [NOT IMPLEMENT]
 
-    #connection.send(str.encode(f'Server is working:'))
-    data = connection.recv(2048)
-    logging.info(f'{address} '+data.decode(FORMAT))
-    response = client_init()
-    connection.sendall(f'{response}'.encode(FORMAT))
-    while True:
-        data = connection.recv(2048)
-        response = '[SERVER] ' +data.decode(FORMAT)
-        if not data:
-            break
-        connection.sendall(str.encode(response))
-
-    connection.close()
-
-def main():
-    thread_count = 0
-    s = start()
-    while True:
-        c, addr = s.accept()
-        logging.debug("Connection from: "+str(addr))
-        start_new_thread(multi_threaded_client, (c,addr))
-        thread_count += 1
-        logging.debug(f"Thread: {thread_count}")
-    s.close()
-
-if __name__ == "__main__":
-    main() 
+if __name__ == '__main__':
+    s = Server(6969)
+    s.start() 
