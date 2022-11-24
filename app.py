991923824e534f26e5bfa4c900ff72d0835f00ed
hxfe <bittersalad@tutanota.de>
2022-11-25 02:50:38 +0700

    logging

--- app.py
+++ app.py
@@ -20,7 +20,17 @@ class Server:
     FORMAT = "utf-8"
 
     def __init__(self, port:int):        
-        logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(asctime)s %(message)s')
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(1)
+        f_handler = logging.FileHandler('app.log')
+        c_handler = logging.StreamHandler()
+        
+        formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(message)s')
+        c_handler.setFormatter(formatter)
+        f_handler.setFormatter(formatter)
+        for h in (c_handler,f_handler):
+            self.logger.addHandler(h)
+        #logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(asctime)s %(message)s')
         self.port = port
         self.lb = lobby.Lobby("status.json")
 
@@ -38,9 +48,9 @@ class Server:
                 # self.connections[c1].sendall(f'{message}'.encode("utf-8")) # ex. {"matchStart":[1,123]}
                 # self.connections[c2].sendall(f'{message}'.encode("utf-8")) # ex. {"matchStart":[1,123]}
                 matched = 0
-                logging.debug("matched!!!")
+                self.logger.debug("matched!!!")
             time.sleep(1)
-        logging.debug("match_request_check has stopped working")    
+        self.logger.debug("match_request_check has stopped working")    
   
     def broadcast(self, msg, client1, client2):
         """
@@ -99,16 +109,15 @@ class Server:
             if not data:
                 print("not data")
                 break
-        stop_threads = False # recv_thread parameter for killing the thread
+        stop_threads = False    
         recv_thread = Thread(target=self.threaded_recieve, args=(c,lambda: stop_threads,))
         recv_thread.start()
         self.threads.append(recv_thread)
-        self.word_gen() # Generated word list to be send to clients
-        stop_threads = True 
-        # Stop thread for all client
+        self.word_gen()
+        stop_threads = True
         for t in self.threads:
             t.join()
-        logging.debug('All reciever threads are dead')    
+        self.logger.debug('All reciever threads are dead')    
         c.close()    
 
     def start(self, client_n:int=2):
@@ -119,13 +128,13 @@ class Server:
         try:
             sock.bind(('',self.port))
         except socket.error as e:
-            logging.error(f"{str(e)}")
-        logging.info(f"socket is binded to {self.port}")
+            self.logger.error(f"{str(e)}")
+        self.logger.info(f"socket is binded to {self.port}")
 
         # listen to n clients 
         sock.listen(client_n)
         self.listening = True
-        logging.info("socket is listening")
+        self.logger.info("socket is listening")
         thread = Thread(target = self.match_request_check,)
         thread.start()
         #start_new_thread(self.match_request_check(),)
@@ -144,13 +153,13 @@ class Server:
             print("") 
             #print(self.connections)
             #print(self.addresses) 
-            logging.debug("Connection from: "+str(self.addr))
+            self.logger.debug("Connection from: "+str(self.addr))
             start_new_thread(self.multi_threaded_client,(self.c, new_client['newClient'])) #use current self.c to start a new thread
             self.thread_count += 1
-            logging.debug(f"Thread: {self.thread_count}")
+            self.logger.debug(f"Thread: {self.thread_count}")
             #logging.debug(f"connections[] length: " + str(len(self.connections))) 
             print("")
-        logging.info("socket is closed")
+        self.logger.info("socket is closed")
         self.listening = False
         sock.close()
     
